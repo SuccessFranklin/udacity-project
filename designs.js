50056a566c99326ad0fd2ce7e64ef757ec16ee8c
SuccessFranklin <successfranklin@gmail.com>
2018-06-30 15:26:01 +0100

    first commit

---
+++ designs.js
@@ -0,0 +1,36 @@
+var table = $("#pixelCanvas");
+var x = ""// Select color input
+var height = "";
+var width = "";// Select size input
+
+// When size is submitted by the user, call makeGrid()
+$("input[type='submit']").on("click",function()
+{
+	height = $("#inputHeight").val();
+	width = $('#inputWeight').val();
+	
+	makeGrid();
+});
+
+function makeGrid(){
+
+	table.children().remove("tr");
+
+	//creaating the rows
+	for(var i=1;i<=height;i++){
+		
+		var row = $("<tr></tr>");
+		
+		for(var j=1;j<=width;j++){
+			row.append("<td></td>");
+		}
+		table.append(row);
+	}
+}
+  
+  
+    table.on("click","td",function(){
+	  x = $('#colorPicker').val();
+	  $(this).css('background-color', x);
+      // Your code goes here!
+    });
